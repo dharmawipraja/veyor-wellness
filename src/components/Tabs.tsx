3ac5be48615f8ca3fdd2dc743452375285d167ae
I Made Budi Dharma Wipraja <v-i.mbwipraja@btpn.com>
2024-10-12 22:30:25 +0700

    fix tab styles

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -13,17 +13,16 @@ type TabsProps = {
 const Tabs: React.FC<TabsProps> = ({ children, activeTab }) => {
   return (
     <div className="mx-auto max-w-prose">
-      <div className="flex border-b border-gray-300">
-        {children.map(child => (
-          <div
-            key={child.props.label}
-            className={`${
-              activeTab === child.props.label ? 'border-b-2 border-b-black text-black' : 'border-b-0 bg-gray-100 text-gray-700'
-            } flex-1 border-y-[1px] border-gray-300 font-medium py-2 first:border-l-[1px] last:border-r-[1px]`}
-          >
-            {child.props.label}
-          </div>
-        ))}
+      <div className="tab">
+        {children.map(child => {
+          const isActiveStyle = activeTab === child.props.label ? 'tab__item is-active' : 'tab__item';
+
+          return (
+            <div className={`sm:w-48 w-36 ${isActiveStyle}`}>
+              {child.props.label}
+            </div>
+          )
+        })}
       </div>
       <div className="py-4">
         {children.map(child => {
